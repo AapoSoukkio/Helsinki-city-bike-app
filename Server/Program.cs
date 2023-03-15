9081fbf0f7326960f5bb4530221544002d4d8129
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-03-15 13:35:44 +0200

    Backend configuration

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Solita.HelsinkiBikeApp.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+// Add allow all CORS policy to allow calls from clients
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+{
+    policy.AllowAnyHeader();
+    policy.AllowAnyMethod();
+    policy.AllowAnyOrigin();
+}));
+
+builder.Services.AddDbContext<BikeContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("BikeContext")));
+
 var app = builder.Build();
+
+
+//NO need for this at least yet
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
+
+//    SeedData.Initialize(services);
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,6 +43,8 @@ else
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
