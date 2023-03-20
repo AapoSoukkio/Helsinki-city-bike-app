0aa95af87d2d72e6ae8a203197762f0d2c0e2f82
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-03-20 16:50:04 +0200

    Backend to fetch data from June journeys

--- Shared/JuneJourney.cs
+++ Shared/JuneJourney.cs
@@ -2,14 +2,13 @@
 {
     public class JuneJourney
     {
-        public int Id { get; set; }
-        public string Departure { get; set; }
-        public string Return { get; set; }
-        public int DepartureStationId { get; set; }
-        public string DepartureStationName { get; set; }
-        public int ReturnStationId { get; set; }
-        public string ReturnStationName { get; set; }
-        public int CoveredDistance { get; set; }
-        public int Duration { get; set; }
+        public string? Departure { get; set; }
+        public string? Return { get; set; }
+        public int? DepartureStationId { get; set; }
+        public string? DepartureStationName { get; set; }
+        public int? ReturnStationId { get; set; }
+        public string? ReturnStationName { get; set; }
+        public int? CoveredDistance { get; set; }
+        public int? Duration { get; set; }
     }
 }
