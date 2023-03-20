17be9ccd8068fcb4ada0747cf497e25ca666ac4e
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-03-20 22:00:01 +0200

    Deleting not necessary references to classes

--- Server/Data/BikeContext.cs
+++ Server/Data/BikeContext.cs
@@ -12,15 +12,9 @@ namespace Solita.HelsinkiBikeApp.Server.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<MayJourney>().HasNoKey();
-            modelBuilder.Entity<JuneJourney>().HasNoKey();
-            modelBuilder.Entity<JulyJourney>().HasNoKey();
             modelBuilder.Entity<Summer21Journey>().HasNoKey();
         }
 
-        public DbSet<MayJourney> MayJourneys { get; set; }
-        public DbSet<JuneJourney> JuneJourneys { get; set; }
-        public DbSet<JulyJourney> JulyJourneys { get; set; }
         public DbSet<BikeStation> BikeStations { get; set; }
         public DbSet<Summer21Journey> Summer21Journeys { get; set; }
     }
