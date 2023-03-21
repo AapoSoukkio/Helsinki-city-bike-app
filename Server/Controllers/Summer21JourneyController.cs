31e79c336680db97558f140f4e1ac6449d6e38e6
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-03-21 14:28:07 +0200

    Endpoint to fetch top 5 most popular return stations for journeys starting from the station

--- Server/Controllers/Summer21JourneyController.cs
+++ Server/Controllers/Summer21JourneyController.cs
@@ -101,5 +101,28 @@ namespace Solita.HelsinkiBikeApp.Server.Controllers
 
             return Ok(Math.Round(averageDistanceInKilometers, 1));
         }
+
+        [HttpGet("top_return_stations")]
+        public async Task<ActionResult<List<BikeStation>>> GetTopReturnStations(int id)
+        {
+            var journeys = await _db.Summer21Journeys
+                                         .Where(j => j.DepartureStationId == id)
+                                         .GroupBy(j => j.ReturnStationId)
+                                         .Select(g => new { StationId = g.Key, Count = g.Count() })
+                                         .OrderByDescending(g => g.Count)
+                                         .Take(5)
+                                         .ToListAsync();
+
+            var stationIds = journeys.Select(j => j.StationId).ToList();
+            var stations = await _db.BikeStations
+                                        .Where(s => stationIds.Contains(s.ID))
+                                        .ToListAsync();
+
+            // Order the stations by the order of the journey count
+            var orderedStations = stationIds.Select(id => stations.First(s => s.ID == id)).ToList();
+
+            return Ok(orderedStations);
+        }
+
     }
 }
