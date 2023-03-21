21744f57bb899af38c55456c51a61affc0c8e592
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-03-21 22:31:23 +0200

    Added test cases for the API endpoints

---
+++ Solita.HelsinkiBikeApp.Tests/Controllers/BikeStationControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Solita.HelsinkiBikeApp.Server.Controllers;
+using Solita.HelsinkiBikeApp.Server.Data;
+using Solita.HelsinkiBikeApp.Shared;
+
+namespace Solita.HelsinkiBikeApp.Tests.Controllers
+{
+    [TestFixture]
+    public class BikeStationControllerTests
+    {
+        private BikeContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<BikeContext>()
+                .UseInMemoryDatabase(databaseName: "BikeStations")
+                .Options;
+
+            _context = new BikeContext(options);
+            _context.BikeStations.Add(new BikeStation { ID = 1, Name = "Station1", Adress = "Address1" });
+            _context.BikeStations.Add(new BikeStation { ID = 2, Name = "Station2", Adress = "Address2" });
+            _context.SaveChanges();
+        }
+
+        //  Get_ReturnsAllDataFromBikeStationTable test method verifies that the Get() method in the
+        //  BikeStationController returns all the datafrom the BikeStation table in the in-memory database
+        [Test]
+        public async Task Get_ReturnsAllDataFromBikeStationTable()
+        {
+            // Arrange
+            var controller = new BikeStationController(null, _context);
+
+            // Act
+            var result = await controller.Get();
+
+            // Assert
+            NUnit.Framework.Assert.IsNotNull(result);
+            NUnit.Framework.Assert.IsInstanceOf<IEnumerable<BikeStation>>(result);
+            NUnit.Framework.Assert.AreEqual(2, result.Count());
+        }
+
+        //The GetStations_ReturnsExpectedResults_WhenStationNameIsProvided() test method verifies that
+        //the GetStations() method in the BikeStationController returns the expected results when a search
+        //term is provided.
+        [Test]
+        public async Task GetStations_ReturnsExpectedResults_WhenStationNameIsProvided()
+        {
+            // Arrange
+            var controller = new BikeStationController(null, _context);
+            string stationName = "Station1";
+
+            // Act
+            var result = await controller.GetStations(stationName);
+
+            // Assert
+            NUnit.Framework.Assert.IsNotNull(result);
+            NUnit.Framework.Assert.IsInstanceOf<ActionResult<IEnumerable<BikeStation>>>(result);
+            NUnit.Framework.Assert.AreEqual(1, result.Value.Count());
+            NUnit.Framework.Assert.AreEqual("Station1", result.Value.First().Name);
+        }
+    }
+}
